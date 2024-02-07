2da66361320f1d9875645d2af8f0a5f3287603ba
Jonny Shen <jonnyshen@Jonnys-MacBook-Pro-2.local>
2024-02-07 11:45:14 -0500

    change

--- affirmative_app/__init__.py
+++ affirmative_app/__init__.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + dbusername + ':' + dbpassword + '@localhost/' + dbname
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'affirmative_app', 'static', 'uploads')
-#app.config['SESSION_COOKIE_DOMAIN'] = 'affirmative-b9753633e8e0.herokuapp.com'
+app.config['SESSION_COOKIE_DOMAIN'] = 'affirmative-b9753633e8e0.herokuapp.com'
 app.config['SESSION_COOKIE_PATH'] = '/'
 app.config['SESSION_COOKIE_NAME'] = 'session'
 app.config['SESSION_COOKIE_HTTPONLY'] = True
-#app.config['SESSION_COOKIE_SECURE'] = True  # Use True for HTTPS
+app.config['SESSION_COOKIE_SECURE'] = True  # Use True for HTTPS
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'  # or 'None' if necessary
 # app.config['SECRET_KEY'] = os.urandom(24)
--- affirmative_app/config.py
+++ affirmative_app/config.py
@@ -9,6 +9,14 @@ class Config(object):
     MYSQL_DATABASE_HOST = 'us-cluster-east-01.k8s.cleardb.net'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://b405e5cd5117bb:b22b9c56@us-cluster-east-01.k8s.cleardb.net/heroku_29aebe8b6ee46de'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optional: Reduces overhead if you don't need it
+    SQLALCHEMY_ENGINE_OPTIONS = {
+    'pool_pre_ping': True,
+    # Optional: other engine options like pool size
+    'pool_size': 10,
+    'max_overflow': 10,
+    'pool_timeout': 30,
+    'pool_recycle': 1800
+    }
 
 # dbusername = 'root'
 # dbpassword = '030129happyjjday'
